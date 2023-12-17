c7a8f49aa648d30f0d280acf36cc25b57f56eb59
Bastien MONTOIS <bastien.montois@gmail.com>
2023-12-17 15:12:48 +0100

    ✨ feat(copy-button.tsx): add visual feedback for copy action with a check mark icon 🐛 fix(copy-button.tsx): replace direct copy function with useCallback to prevent unnecessary re-renders 🔧 refactor(copy-button.tsx): use useEffect to reset copy state after 2 seconds for better user experience

--- src/components/copy-button.tsx
+++ src/components/copy-button.tsx
@@ -1,26 +1,39 @@
 'use client'
 
+import { useCallback, useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { useToast } from '@/components/ui/use-toast'
-import { Files } from 'lucide-react'
+import { Check, Files } from 'lucide-react'
 
 type ButtonCopyProps = {
   text: string
 }
 
 export const ButtonCopy = ({ text }: ButtonCopyProps) => {
+  const [hasCopied, setHasCopied] = useState(false)
+
   const { toast } = useToast()
 
-  const copyText = () => {
-    navigator.clipboard.writeText(text)
-    toast({
-      title: 'Copied !',
-    })
-  }
+  useEffect(() => {
+    setTimeout(() => {
+      setHasCopied(false)
+    }, 2000)
+  }, [hasCopied])
+
+  const copyToClipboard = useCallback((value: string) => {
+    navigator.clipboard.writeText(value)
+    toast({ title: 'Copied!' })
+    setHasCopied(true)
+  }, [])
 
   return (
-    <Button onClick={copyText} disabled={!text}>
-      Copy <Files className="w-4 h-4 ml-2" />
+    <Button onClick={() => copyToClipboard(text)}>
+      Copy
+      {hasCopied ? (
+        <Check className="w-4 h-4 ml-2" />
+      ) : (
+        <Files className="w-4 h-4 ml-2" />
+      )}
     </Button>
   )
 }
